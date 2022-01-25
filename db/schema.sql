c448746380481681fa16b063f16822d1a25c9226
Labrot <sarah.lientz@gmail.com config --global user.email>
2022-01-24 20:06:49 -0500

    finally got schema and seeds files working

--- db/schema.sql
+++ db/schema.sql
@@ -1,33 +1,35 @@
 /* To create database run the scripts in this file */
-DROP DATABASE IF EXISTS employeeDB;;
-
+DROP DATABASE IF EXISTS employeeDB;
 CREATE DATABASE employeeDB;
 
 USE employeeDB;
 
-CREATE TABLE department(
+CREATE TABLE departments (
 id INT AUTO_INCREMENT NOT NULL,
 name VARCHAR(30) NOT NULL,
 PRIMARY KEY(id)
 );
 
-USE employeeDB;
+
 
 CREATE TABLE roles (
 id INT AUTO_INCREMENT NOT NULL,
-title VARCHAR(30) NOT NULL,
-salary DECIMAL NOT NULL,
-department_id INT NOT NULL,
-PRIMARY KEY(id)
+title VARCHAR(30),
+salary DECIMAL(10.3) NULL,
+department_id INT NULL,
+PRIMARY KEY(id),
+FOREIGN KEY(department_id) REFERENCES departments(id)
 );
 
-USE employeeDB;
+
 
 CREATE TABLE employees (
 id INT AUTO_INCREMENT NOT NULL,
-first_name VARCHAR(30) NOT NULL,
-last_name VARCHAR(30) NOT NULL,
-role_id INT NOT NULL,
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+role_id INT,
 manager_id INT,
-PRIMARY KEY(id)
+PRIMARY KEY(id),
+FOREIGN KEY(manager_id) REFERENCES employees(id) ON DELETE SET NULL,
+FOREIGN KEY(role_id) REFERENCES roles(id) ON DELETE SET NULL
 ); 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,28 @@
-INSERT INTO departments(name) VALUES("marketing");
-INSERT INTO departments(name) VALUES("sales");
-INSERT INTO departments(name) VALUES("support");
+INSERT INTO departments (name) 
+ VALUES ('Marketing'),
+        ('Customer Support'),
+        ('Sales'),
+        ('Product Development');
+        
 
-INSERT INTO roles(title, salary, department_id) VALUES("Manager", 700000.00, 1);
-INSERT INTO roles(title, salary, department_id) VALUES("Intern", 200000.00, 2);
-INSERT INTO roles(title, salary, department_id) VALUES("Employee", 500000.00, 3);
 
-INSERT INTO employees (first_name, last_name, role_id) VALUES("Clapton","Eric", 3);
-INSERT INTO employees (first_name, last_name, role_id) VALUES("Wilde","Olivia", 2);
-INSERT INTO employees (first_name, last_name, role_id) VALUES("Bowie","David", 1);
+INSERT INTO roles (title, salary, department_id) 
+ VALUES ('Sales Manager', 300000, 3),
+        ('Accountant', 100000, 4),
+        ('Office Manager', 200000, 2),
+        ('Marketing Manager', 180000, 1);
+
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id) 
+ VALUES ('David','Bowie',  1, NULL),
+        ('Eric', 'Clapton', 4, 1),
+        ('Gene', 'Wilder', 2, NULL),
+        ('Susy', 'Que', 1, 3);     
+
+
+
+
+
+
+
+
